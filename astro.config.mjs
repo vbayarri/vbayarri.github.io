8981a3ec3c9754d3212c826667ad9420e284868c
Víctor Bayarri Andrés <4997837+vbayarri@users.noreply.github.com>
2025-04-17 13:26:18 +0200

    Upgraded fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://vbayarri.github.io',
+	site: 'https://vbayarri.github.io',
+	integrations: [mdx(), sitemap()],
 });
